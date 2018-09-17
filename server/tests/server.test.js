50ef29eb215aeb27d7f504b16f9544cf336b8205
Snir Koppelman <snirkwork@gmail.com>
2018-09-17 19:57:38 +0300

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,42 @@ describe('GET /todos/id', () => {
             .end(done)
     });
 });
+
+describe('DELETE /todos:id', () => {
+    it('should remove a todo', done => {
+        let hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo._id).toBe(hexId)
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+
+                // query db using findbyid, tonoexist
+                //expect(null).tonotexist();
+                Todo.findById(hexId).then(todo => {
+                    expect(todo).toNotExist;
+                    done();
+                }).catch(e => done(e))
+            });
+    });
+
+    it('should return 404 if todo not found', done => {
+        let fakeId = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${fakeId}`)
+            .expect(404)
+            .end(done)
+    });
+
+    it('should return 404 if object id is invalid', done => {
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done)
+    });
+})
